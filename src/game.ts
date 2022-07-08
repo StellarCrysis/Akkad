06162a4ae081a6a830f464f1f8beb7716e37abe6
Grabli66 <grabli66@gmail.com>
2022-07-08 22:49:19 +0300

    Доработка класса игры и состояния

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,34 @@
+import * as BABYLON from "babylonjs"
+import { GameState } from "./gamestate"
+
 // Основной класс игры
 export class Game {
+    // Инстанс игры
+    static instance = new Game()
+
+    // Текущее состояние
+    private _state: GameState = null!
+
+    // HTML канвас игры
+    private canvas: HTMLCanvasElement
+
+    // Игровой движок
+    private engine: BABYLON.Engine
+
+    // Конструктор
+    private constructor() {
+        this.canvas = document.createElement("canvas")
+        this.engine = new BABYLON.Engine(this.canvas, true)
+    }
+
+    // Устанавливает состояние
+    setState(state: GameState): Promise<void> {
+        // Освобождает предыдущее состояние
+        if (this._state != null) {
+            this._state.dispose()
+        }
+
+        this._state = state
+        return state.enter()
+    }
 }
--- src/gamestate.ts
+++ src/gamestate.ts
@@ -1,4 +1,13 @@
-// Состояние игры
-export class GameState {
+import * as BABYLON from "babylonjs"
 
+// Состояние игры
+export abstract class GameState implements BABYLON.IDisposable {
+    // Основная сцена
+    private _scene: BABYLON.Scene
+
+    // Обрабатывает вход в состояние
+    abstract enter(): Promise<void>
+
+    // Освобождает ресурсы
+    abstract dispose(): void
 }
